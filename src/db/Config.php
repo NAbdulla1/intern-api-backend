727043d3c99cf7acf2882d54f2799a9fad7b556e
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-04-22 13:04:45 +0600

    set up DB connection

---
+++ src/db/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database;
+
+class Config
+{
+    public const HOST = "localhost";
+    public const PORT = "3306";
+    public const USER_NAME = "abdulla-intern";
+    public const USER_PASSWORD = "0]MiHcN*kY@E]p0g";
+    public const DB_NAME = "intern-db";
+}
---
+++ src/db/DB.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Database;
+use mysqli;
+
+require "../../vendor/autoload.php";
+
+class DB
+{
+    private static $connection = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getConnection(): mysqli
+    {
+        if (self::$connection == null) {
+            self::$connection = new mysqli(
+                config::HOST,
+                config::USER_NAME,
+                config::USER_PASSWORD,
+                config::DB_NAME,
+                config::PORT);
+        }
+        return self::$connection;
+    }
+}
+
+DB::getConnection();
