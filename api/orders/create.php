d70096d510c4c4c3e83def6e55c30d0870b35619
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-04-28 09:40:02 +0600

    make sure only customers can order products, admins can't

--- api/orders/create.php
+++ api/orders/create.php
@@ -2,6 +2,8 @@
 
 use Controller\OrderController;
 use JSON_Web_Token\JWTController;
+use Utils\OtherResponse;
+use Utils\ResponseCodes;
 
 require "../../vendor/autoload.php";
 
@@ -10,6 +12,10 @@ require "../../vendor/autoload.php";
 $access_token = isset(apache_request_headers()['access_token']) ? apache_request_headers()['access_token'] : "";
 $user = JWTController::validateToken($access_token);
 if (!$user) exit();
+if($user->isAdmin()){
+    OtherResponse::send(ResponseCodes::HTTP_FORBIDDEN, "Only Customers are allowed to create orders");
+    exit();
+}
 
 $content = json_decode(file_get_contents("php://input"), true);
 $content = (empty($content) || empty($content['product_sku'])) ? null : $content;
