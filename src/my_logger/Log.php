6445dc0e3765d6ac04044feb8445e84d4218c3f6
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-04-30 12:31:53 +0600

    add a logger

---
+++ src/my_logger/Log.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyLogger;
+
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class Log
+{
+    private static Logger $logger;
+
+    private function __construct()
+    {
+    }
+
+    public static function dbg($message)
+    {
+        if (!isset(self::$logger)) {
+            self::$logger = new Logger('debug-channel');
+            self::$logger->pushHandler(new StreamHandler(__DIR__ . "/../../logs.log", Logger::DEBUG));
+        }
+        self::$logger->debug($message);
+    }
+}
