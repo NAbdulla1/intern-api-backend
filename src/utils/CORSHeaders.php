f005149a319a7058b3684c292e018ca52d7384ad
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-01 13:01:54 +0600

    add comments clarifying header purpose

--- src/utils/CORSHeaders.php
+++ src/utils/CORSHeaders.php
@@ -9,11 +9,11 @@ class CORSHeaders
     static function setProperResponseHeadersForCors(): void
     {
         http_response_code(200);
-        header("Access-Control-Allow-Origin: http://localhost:3000");
-        header("Access-Control-Expose-Headers: Content-Length");
+        header("Access-Control-Allow-Origin: http://localhost:3000");// the origin to allow access from to this api
+        header("Access-Control-Expose-Headers: Content-Length");// send content-length as response to the request
         if (strcasecmp($_SERVER['REQUEST_METHOD'], 'options') == 0) {
-            header("Access-Control-Allow-Headers: Content-Type,access_token");
-            header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");
+            header("Access-Control-Allow-Headers: Content-Type,access_token");// as response to Access-Control-Request-Headers of preflight request
+            header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");// as response to Access-Control-Request-Method of preflight request
             header('Access-Control-Max-Age: 3600');
             exit(0);//early exit if the request is a preflight check ie. OPTIONS request before original request, otherwise a 400 bad request will be thrown
         }
