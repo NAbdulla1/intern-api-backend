e8fd4a10028abfe7b6cfb6fb70660458ff636981
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-01 12:54:31 +0600

    set Access-Control-Max-Age to 1 hour to cache permission

--- src/utils/CORSHeaders.php
+++ src/utils/CORSHeaders.php
@@ -10,11 +10,13 @@ class CORSHeaders
     {
         http_response_code(200);
         header("Access-Control-Allow-Origin: http://localhost:3000");
-        header("Access-Control-Allow-Headers: Content-Type,access_token");
-        header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");
         header("Access-Control-Expose-Headers: Content-Length");
-        if (strcasecmp($_SERVER['REQUEST_METHOD'], 'options') == 0)
+        if (strcasecmp($_SERVER['REQUEST_METHOD'], 'options') == 0) {
+            header("Access-Control-Allow-Headers: Content-Type,access_token");
+            header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");
+            header('Access-Control-Max-Age: 3600');
             exit(0);//early exit if the request is a preflight check ie. OPTIONS request before original request, otherwise a 400 bad request will be thrown
+        }
         header("Content-Type: application/json");
     }
 }
