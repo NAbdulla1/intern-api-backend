7071344633086d83cc1107d300244ec6c1ae501b
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-01 11:43:04 +0600

    fix CORS related problem

--- src/utils/CORSHeaders.php
+++ src/utils/CORSHeaders.php
@@ -8,11 +8,14 @@ class CORSHeaders
 {
     static function setProperResponseHeadersForCors(): void
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: Access-Control-Request-Method,Access-Control-Allow-Headers,Origin,Content-Type,access_token,Access-Control-Request-Headers");
-        header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");
-        header("Access-Control-Expose-Headers: Content-Length");
-        header("Content-Type: application/json");
         http_response_code(200);
+        if (strcasecmp($_SERVER['REQUEST_METHOD'], 'options') == 0) {
+            header("Access-Control-Allow-Origin: http://localhost:3000");
+            header("Access-Control-Allow-Headers: Origin,Content-Type,access_token");
+            header("Access-Control-Allow-Methods: GET,POST,DELETE,PATCH,OPTIONS,PUT,HEAD");
+            header("Access-Control-Expose-Headers: Content-Length");
+            exit(0);//early exit if the request is a preflight check, otherwise a 400 bad request will be thrown
+        }
+        header("Content-Type: application/json");
     }
 }
